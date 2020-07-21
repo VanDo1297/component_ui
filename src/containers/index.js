47851f1e7a33fede11827f0e412f54f1ce804264
do <ngovando261297@gmail.com>
2020-07-21 09:44:23 +0700

    add-fontanwesome-and-bootstrap

---
+++ src/containers/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+export default function App() {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faCoffee} />
+    </div>
+  );
+}
